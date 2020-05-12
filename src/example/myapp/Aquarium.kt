37492b25147ad6fa64c607b533fe59bc65576cb1
pizzacrizza <ivan.alineri@outlook.com>
2020-05-12 14:30:42 -0300

    Estudos parte 1

---
+++ src/example/myapp/Aquarium.kt
@@ -0,0 +1,23 @@
+package example.myapp
+
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40){
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} l")
+    }
+
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+}
